ac071d85263efd6978993c490201ca2a098cd181
Andres <ajramirez@us.es>
2020-09-07 13:00:58 +0000

    primer commit

---
+++ docker/docker-settings1.py
@@ -0,0 +1,42 @@
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'localhost',
+        'PORT': 5432,
+    }
+}
+
+STATIC_ROOT = '/app/static/'
+MEDIA_ROOT = '/app/static/media/'
+ALLOWED_HOSTS = ['*']
+
+# Modules in use, commented modules that you won't use
+MODULES = [
+    'authentication',
+    'base',
+    'booth',
+    'census',
+    'mixnet',
+    'postproc',
+    'store',
+    'visualizer',
+    'voting',
+]
+
+BASEURL = 'http://localhost:8000'
+
+APIS = {
+    'authentication': localhost,
+    'base': localhost,
+    'booth': localhost,
+    'census': localhost,
+    'mixnet': localhost,
+    'postproc': localhost,
+    'store': localhost,
+    'visualizer': localhost,
+    'voting': localhost,
+}
